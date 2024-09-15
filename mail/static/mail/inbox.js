c0f846be37a0d05486b424f679f7ad7d06e4bc0d
Mokhtar <mada200888@gmail.com>
2024-09-15 21:32:29 +0300

    view sent, inbox and archived emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -63,4 +63,48 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // make an api call to get all the emails
+  if(mailbox === 'inbox'){
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+
+        // Iterate on every email and display it      
+        emails.forEach(elemento => {
+          const element = document.createElement('div');
+          element.innerHTML = `${elemento.sender}  ${elemento.subject}  ${elemento.body}`;
+          document.querySelector('#emails-view').append(element);
+        });
+        
+    });
+  }
+  if(mailbox === 'archive'){
+    fetch('/emails/archive')
+    .then(response => response.json())
+    .then(emails => {
+
+        // Iterate on every email and display it      
+        emails.forEach(elemento => {
+          const element = document.createElement('div');
+          element.innerHTML = `${elemento.sender}  ${elemento.subject}  ${elemento.body}`;
+          document.querySelector('#emails-view').append(element);
+        });
+        
+    });
+  }
+  if(mailbox === 'sent'){
+    fetch('/emails/sent')
+    .then(response => response.json())
+    .then(emails => {
+
+        // Iterate on every email and display it      
+        emails.forEach(elemento => {
+          const element = document.createElement('div');
+          element.innerHTML = `${elemento.sender}  ${elemento.subject}  ${elemento.body}`;
+          document.querySelector('#emails-view').append(element);
+        });
+        
+    });
+  }
 }
